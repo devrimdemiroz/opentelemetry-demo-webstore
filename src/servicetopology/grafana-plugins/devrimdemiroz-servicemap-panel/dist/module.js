025e158d6a06ddd9d73be5c320974bda8b94eaea
devrimdemiroz <devrim.demiroz@gmail.com>
2023-03-18 12:24:01 +0100

    Opentelemetry Collector Data Flow

---
+++ src/servicetopology/grafana-plugins/devrimdemiroz-servicemap-panel/dist/module.js
@@ -0,0 +1,34 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+define(() => { return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./module.ts":
+/*!*******************!*\
+  !*** ./module.ts ***!
+  \*******************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ../node_modules/swc-loader/src/index.js):\nError: ENOENT: no such file or directory, open '/Users/taadeul1/otel/opentelemetry-demo/src/servicetopology/grafana-plugins/devrimdemiroz-servicemap-panel/src/module.ts'");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./module.ts"]();
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});;
